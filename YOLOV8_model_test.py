5cd40b7de7a497585325b73a5463f831f0733ab6
Dorian-MB <dorian.mariani@gmail.com>
2023-05-29 18:20:54 +0200

    update

--- YOLOV8_model_test.py
+++ YOLOV8_model_test.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 model_path = "models/nano_189epochs_SGD.pt"
 model = YOLO(model_path)
 
-source = "input/video_test_5.mp4"
+source = "input/test_outlet_03.mp4" # test_outlet_03
 # source = 0 # for live detection (webcam needed)
 result = model.predict(source=source, save=True)
